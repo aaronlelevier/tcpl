0a67c37d236c27f9048855b48ae4f39cfd89a3a9
aaronlelevier <aaron.lelevier@gmail.com>
2022-08-09 08:06:59 -0700

    can print longest line

--- tcpl/ex_1_16_longest_line.c
+++ tcpl/ex_1_16_longest_line.c
@@ -5,26 +5,25 @@
 
 #define MAX_LINE_LENGTH 80
 
-void printArgv(char **argv)
-{
-    printf("Args count: %ld\n", sizeof argv);
-    for (int i = 0; i < sizeof argv; ++i)
-        printf("Args idx:%d arg:%s\n", i, argv[i]);
-}
-
 int main(int argc, char **argv)
 {
     char *path;
+    char *longestLine = malloc(MAX_LINE_LENGTH);
     char line[MAX_LINE_LENGTH] = {0};
+    int longestLength = 0;
+    // TODO: how to declare multiple vars on a single line
     unsigned int line_count = 0;
-
-    printf("")
-
-    printArgv(argv);
 
     if (argc < 1)
         return EXIT_FAILURE;
-    path = argv[1];
+
+    // TODO: how to access $HOME env var
+    printf("HOME: %s\n", getenv("HOME"));
+
+    // TODO: how to join a file path
+
+
+    path = "/Users/aaron/Desktop/ch5-ex1-config.json";
 
     /* Open file */
     FILE *file = fopen(path, "r");
@@ -38,13 +37,25 @@ int main(int argc, char **argv)
     /* Get each line until there are none left */
     while (fgets(line, MAX_LINE_LENGTH, file))
     {
+        if (strlen(line) > longestLength) {
+            // TODO: string interpolation for unsigned int
+            printf("found longest len[%d]: line: %s\n",
+                   (int) strlen(line), line);
+            strcpy(longestLine, line);
+            longestLength = strlen(line);
+        }
+
         /* Print each line */
-        printf("line[%06d]: %s", ++line_count, line);
+        printf("line[%06d]: len[%d]: %s",
+               ++line_count, (int) strlen(line), line);
 
         /* Add a trailing newline to lines that don't already have one */
         if (line[strlen(line) - 1] != '\n')
             printf("\n");
     }
+
+    // print longest line
+    printf("len[%d]: %s", longestLength, longestLine);
 
     /* Close file */
     if (fclose(file))
